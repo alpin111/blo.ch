361a3f63dca9c9bb4d9077893ae71bac599edce4
Kevin Bloch <Kevin@Kevins-MacBook-Air.local>
2017-06-16 13:26:00 +0200

    c

---
+++ js/gallery.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+var dir = "portfoliofolder/" + slice;
+var fileextension = ".jpg";
+
+   var count;
+
+   for(count = 1; count < 17; count++){
+      document.write('<object data="portfoliofolder/'+
+                      slice +
+                      '/'+
+                      count +
+                      '.jpg"  type="image/jpg"><img src="media/default.png" ></object><br>');
+
+   }
