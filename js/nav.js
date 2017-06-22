bfac5f4ec485fd72b4deef8b4d54553ee788eaf5
Kevin Bloch <Kevin@Kevins-MacBook-Air.local>
2017-06-22 18:50:46 +0200

    m

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,7 @@
 document.write(
 '<header>'+
 '<nav><ul>'+
-'<li id="index"><a href="index">Home</a></li>'+
+'<li id="index"><a href="/">Home</a></li>'+
 '<li id="portfolio"><a href="portfolio">Portfolio</a></li>'+
 '<li id="about"><a href="about">About</a></li>'+
 '<li id="contact"><a href="contact">Contact</a></li>'+
