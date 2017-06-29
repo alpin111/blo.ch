2abe01aacb31ad62fe7bf186b745ca4d2c035a99
Kevin Bloch <Kevin@Kevins-MacBook-Air.local>
2017-06-29 16:21:48 +0200

    store optimized

--- js/store.js
+++ js/store.js
@@ -1,186 +1,172 @@
 
-var project_list = [] ;
+ project_list = [
 
 
 
 
-var audi = {
+ audi = {
   string : "audi",
  client : "Audi",
  name : "Casemovie Explains Men",
  project : "A casemovie about the Audi RS 4 mailing and deep zoom banner campaign.",
  special : "It points out mens' fascination for cars and goes on to explain how the campaign exploits the male brain.",
  images : 0,
- };
- project_list.push(audi);
+ },
 
 
 
-var colgate = {
+ colgate = {
 string : "colgate",
 client : "Colgate-Palmolive",
 name : "Time-ticking Campaign",
 project : "A campaign with a contest to popularize Colgate's 12-hour protection toothpaste.",
 special : "To emphasize the 12-hour lasting protection of Colgate's toothpaste, the contest required participants to repost the contest every 12 hours. The longest reposting-chain won.",
 images : 8,
-};
-
-project_list.push(colgate);
+},
 
 
-var palmolive = {
+
+ palmolive = {
   string : "palmolive",
  client : "Colgate-Palmolive",
  name : "Campaign for your Hands",
  project : "A cross-media campaign to advertise Palmolive's soap by explaining the importance of washing your hands.",
  special : "The campaign also uses QR codes and cameras on germ-rich spots to demonstrate how often we pick up bacteria.",
  images : 7,
- };
-
-project_list.push(palmolive);
+ },
 
 
 
 
-var dksh = {
+
+ dksh = {
    string : "dksh",
   client : "DKSH",
   name : "Asia Print Ads",
   project : "A print ad campaign that demonstrates DKSH's footing in Asia.",
   special : "",
   images : 1,
-  };
-
-project_list.push(dksh);
+  },
 
 
-var marionnaud = {
+
+ marionnaud = {
    string : "marionnaud",
   client : "Marionnaud",
   name : "Print Ads for your Mother",
   project : "A mailing sent on Mother's Day.",
   special : "Being provided with almost no information about the perfumes, I came up with my own psychedelic descriptions",
   images : 4,
-  };
-
-project_list.push(marionnaud);
+  },
 
 
-var microsoft = {
+
+ microsoft = {
    string : "microsoft",
   client : "Microsoft",
   name : "Mailing for Working People",
   project : "A Mailing adressed at small and medium businesses explaining the use of Microsoft's new software tools.",
-  special : "The mailing was personalized for various profession groups to demonstrate how Microsoft's new software adds to their existing tools.",
+  special : "The mailing was personalized for ious profession groups to demonstrate how Microsoft's new software adds to their existing tools.",
   images : 4,
-  };
+  },
 
- project_list.push(microsoft);
 
-var microsoft2 = {
+ microsoft2 = {
    string : "microsoft2",
   client : "Microsoft",
   name : "Mailing from the Future",
   project : "A mailing that was seemlessly integrated in Bilanz, a financial magazine.",
   special : "This issue of Bilanz contained a special article from the future for subscribers, telling the story of how this very article changed their life by convincing them to invest in cloud computing.",
   images : 4,
-  };
+  },
 
-project_list.push(microsoft2);
 
-var migros = {
+ migros = {
   string : "migros",
  client : "Migros",
  name : "Interactive Hiking Trails",
  project : "A campaign with a contest to advertise Migros' Heidi product line.",
  special : "The contest includes an application that allows marking benches in the mountain areas.",
  images : 13,
- };
+ },
 
-project_list.push(migros);
 
-var mobimo = {
+ mobimo = {
    string : "mobimo",
   client : "Mobimo",
   name : "Tower Website",
   project : "A website advertising the new flats of the Mobimo Tower.",
   special : "The new website was combined with an AdWords campaign. To this day, all flats are rented out except one or two on the top floors.",
   images : 8,
-  };
+  },
 
-project_list.push(mobimo);
 
-var neocitran = {
+ neocitran = {
   string : "neocitran",
- client : "Novartis",
+ client : "Notis",
  name : "Cuckatoo Campaign",
  project : "A campaign aimed to give NeoCitran the edge over its competitors.",
  special : "For some reason, the campaign was required to include a cuckatoo.",
  images : 10,
- };
-
-project_list.push(neocitran);
+ },
 
 
 
-var novartis = {
-   string : "novartis",
-  client : "Novartis",
+
+ notis = {
+   string : "notis",
+  client : "Notis",
   name : "Global Master Website",
   project : "A new website that was to serve as a template for all subsidiaries.",
   special : "The content was written in cooperation with doctors and trainers to ensure medical accuracy.",
   images : 15,
-  };
-
-project_list.push(novartis);
+  },
 
 
-var novartis2 = {
-   string : "novartis2",
-  client : "Novartis",
+
+ notis2 = {
+   string : "notis2",
+  client : "Notis",
   name : "Internal Communication",
-  project : "A video and internal newsletter informing novartis employees worldwide of their new Voltaren website.",
+  project : "A video and internal newsletter informing notis employees worldwide of their new Voltaren website.",
   special : "The tonality of the video aims for 'American enthusiasm', adressing the culture of the Voltaren management.",
   images : 1,
-  };
-
- project_list.push(novartis2);
+  },
 
 
 
-var rog = {
+
+ rog = {
    string : "rog",
   client : "Reporters without borders",
   name : "Putin Video Ad",
   project : "A video raising awareness of the journalists' rights being threatened.",
   special : "",
   images : 0,
-  };
-
- project_list.push(rog);
+  },
 
 
 
-var schindler = {
+
+ schindler = {
   string : "schindler",
  client : "Schindler",
  name : "Image Material",
  project : "Posters for an event hosted by Schindler.",
  special : "",
  images : 2,
- };
-
- project_list.push(schindler);
+ },
 
 
 
-var valora = {
+
+ valora = {
   string : "valora",
  client : "Valora",
  name : "Product Rebranding",
  project : "Renaming Valora's ok.- product line.",
  special : "The products are not just ok, they each deserve their own adjective.",
  images : 2,
- };
+ },
 
- project_list.push(valora);
+] ;
